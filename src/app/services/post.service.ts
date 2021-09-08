1e08bd5a845c342061b5c06833e202bddcb41b28
aydin <aydinterzi7@gmail.com>
2021-09-08 12:22:34 +0300

    params service olusturuldu

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Posts } from '../Models/posts';
@@ -10,8 +10,17 @@ import { AuthService } from './auth.service';
 export class PostService {
   baseUrl:string="https://localhost:44331/api/Posts"
   constructor(private http:HttpClient,private authservice:AuthService) { }
-  getPosts():Observable<Posts[]>{
-  return this.http.get<Posts[]>(this.baseUrl);
+  getPosts(postParams?):Observable<Posts[]>{
+    let params=new HttpParams();
+    if(postParams!=null){
+      if(postParams.category!=null)
+        params.append("category",postParams.category);
+      if(postParams.category!=null)
+        params.append("language",postParams.language);
+      if(postParams.category!=null)
+        params.append("progLanguage",postParams.progLanguage);
+    }
+  return this.http.get<Posts[]>(this.baseUrl,{params:params});
   }
   addPost(post:Posts):Observable<Posts>{
     post.userId=this.authservice.decodedToken.nameid;
